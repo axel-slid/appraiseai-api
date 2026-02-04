7306c987c620e57b447e00ef1d11b87f6903a5d5
Alex Dils <123800824+axel-slid@users.noreply.github.com>
2026-02-04 02:50:30 -0800

    Add CORS origin for appraiseai API

--- api/main.py
+++ api/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:8080",
+        "https://appraiseai-api.onrender.com/",
         "http://127.0.0.1:8080",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
